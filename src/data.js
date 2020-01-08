aeb97f87fa119e52be905bb1dc7622b89c5ac657
Ruth Maureira <ruthalemau@gmail.com>
2020-01-08 13:31:54 -0300

    Interfaz

--- src/data.js
+++ src/data.js
@@ -1,7 +1,64 @@
-/* Manejo de data */
+/* Ordenar alfabéticamente, ID, altura y peso */
 
-// esta es una función de ejemplo
+/* A-Z: Si a<b return -1, se sitúa a en un indice menor que b. Es decir, a viene primero.
+Si a=b retorna 0, se deja a y b sin cambios entre ellos, pero ordenados con respecto a todos los elementos diferentes.
+Si a>b return 1, se sitúa b en un indice menor que a.*/
 
-export const example = () => {
-  return 'example';
+export let orderMovie = (dataMovie, sortBy, sortOrder) => {
+
+  /*Ordenar alfabeticamente */
+  if (sortBy === "title" && sortOrder === "A-Z") {
+    return dataMovie.sort(function(a, b) {
+      if (a.Title == b.Title) {
+        return 0;
+      }
+      if (a.Title > b.Title) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  } else if (sortBy === "title" && sortOrder === "Z-A") {
+    return dataMovie.sort(function(a, b) {
+      if (b.Title == a.Title) {
+        return 0;
+      }
+      if (b.Title > a.Title) {
+        return 1;
+      } else {
+
+        return -1;
+      }
+    });
+  }
+
+  /* Ordenar por id */
+  else if (sortBy == "id" && sortOrder == "asc") {
+    return dataMovie.sort((a, b) => (a.imdbID - b.imdbID));
+  } else if (sortBy == "id" && sortOrder == "desc") {
+    return dataMovie.sort((a, b) => (b.imdbID - a.imdbID));
+  }
+
+  /* Ordenar por popularidad */
+  else if (sortBy == "rating" && sortOrder == "asc") {
+    return dataMovie.sort((a, b) => (parseFloat(a.imdbRating) - parseFloat(b.imdbRating)));
+  } else if (sortBy == "rating" && sortOrder == "desc") {
+    return dataMovie.sort((a, b) => (parseFloat(b.imdbRating) - parseFloat(a.imdbRating)));
+  }
+
+  /* Ordenar por mejor o peor crítica - calificación */
+  else if (sortBy == "metascore" && sortOrder == "asc") {
+    return dataMovie.sort((a, b) => (parseFloat(a.Metascore) - parseFloat(b.Metascore)));
+  } else if (sortBy == "metascore" && sortOrder == "desc") {
+    return dataMovie.sort((a, b) => (parseFloat(b.Metascore) - parseFloat(a.Metascore)));
+  }
+
+  /*Ordenar por año */
+  else if (sortBy == "year" && sortOrder == "asc") {
+    return dataMovie.sort((a, b) => (a.Year - b.Year));
+  } else if (sortBy == "year" && sortOrder == "desc") {
+    return dataMovie.sort((a, b) => (b.Year - a.Year));
+  }
+
+
 };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,69 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+import { orderMovie } from './data.js'; // Importar la función qe está en data.js
 
-import { example } from './data.js';
 
-console.log(example);
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+/*---------------------------------------------------------------------------------*/
+
+/* API Omdb */
+
+/*---------------------------------------------------------------------------------*/
+
+/* Ocultar carrusel al ingresar a página */
+
+/* Ocultar carrusel  al hacer click en buscar*/
+const carouselStart = document.getElementById("carouselStart");
+const btnSearch = document.getElementById("btnSearch");
+
+btnSearch.addEventListener('click', hideCarousel);
+
+function hideCarousel(){
+  carouselStart.style.display = "none";
+}
+
+
+
+
+/*---------------------------------------------------------------------------------*/
+
+/* Buscar título */
+
+//
+//
+// function renderMovie(image, title, year, production) {
+//   const movieTpl = document.getElementById("movieCardTpl");
+//
+//   let movieEl = document.createElement("div");
+//
+//   movieEl.innerHTML = movieTpl.innerHTML;
+//
+//   movieEl.querySelector(".card-img-top").src = image;
+//   movieEl.querySelector(".card-title").innerHTML = title + " " +production;
+//   movieEl.querySelector(".card-text").innerHTML = "Year: " + year;
+//   document.querySelector(".card-columns").appendChild(movieEl);
+// }
+//
+// const searchTitle = document.getElementById("searchInput").value;
+//
+// /* Realizar llamado API */
+//
+// const fetchUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=90f8aca6`;
+// const searchParam = "s=amor&type=series";
+//
+// fetch(`${fetchUrl}&${searchParam}`)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(json) {
+//     console.log(json.Response)
+//     const results = json.Search;
+//
+//     for (let i = 0; i < results.length; i++) {
+//       const movie = results[i];
+//
+//       renderMovie(movie.Poster, movie.Title, movie.Year, movie.imdbID);
+//     }
+//   });
+
+
+
+/*---------------------------------------------------------------------------------*/
